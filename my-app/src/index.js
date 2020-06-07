ecd9f13ebfc2efab93f839cc20a51bac4e9a5566
saharama <saharama@hawaii.edu>
2020-06-06 23:12:12 -1000

    setup react template

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class RHSAAHawaii extends React.Component {
+
+  render() {
+    return (
+        <h1>RHSAA Hawaii!</h1>
+  );
+  }
+}
+
+ReactDOM.render(<RHSAAHawaii />, document.getElementById('root'));
