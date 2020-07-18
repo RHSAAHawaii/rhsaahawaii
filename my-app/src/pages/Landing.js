0540eaf75a4fbe376eb9aec6d9a37a1242bc37fb
saharama <saharama@hawaii.edu>
2020-07-18 01:49:24 -1000

    updated landing and scholarships

--- my-app/src/pages/Landing.js
+++ my-app/src/pages/Landing.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import '../style.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Container, Image, Segment, Modal, Button, Header, Grid, Icon } from 'semantic-ui-react';
-import rhsPhoto from '../images/landing.jpg';
+import rhsPhoto from '../images/landing2.jpg';
 import ogawa from '../images/headshot-ogawa.jpg';
 import test from '../images/rhsaa-logo-test.jpg';
+import projectGrad from '../images/projectgrad-temp.jpg';
 
 const srcRHSLogo = 'http://rhsaahawaii.org/wp-content/uploads/2019/04/cropped-logo2-2.png';
 
 class Landing extends React.Component {
+  state = { modalOpen: false }
+  handleOpen = () => this.setState({ modalOpen: true })
+  handleClose = () => this.setState({ modalOpen: false })
+
   render() {
     const segmentStyle = { margin: '5px 0 0 0', padding: '40px'};
     const gridStyle = { height: "500px" }
@@ -18,28 +23,33 @@ class Landing extends React.Component {
           <Container>
             <Grid container verticalAlign="middle" style={gridStyle}>
               <Grid.Row/>
-              <Grid.Row/>
-              <Grid.Row/>
-              <Grid.Row columns={4} centered color='black'>
-                <Grid.Column width={3}>
-                  <Image src={srcRHSLogo} size='large'/>
-                </Grid.Column>
-                <Grid.Column width={5}>
-                  <Header as='h1' inverted>Roosevelt High School<br/>Alumni Association</Header>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row columns={1} centered color='black'>
-                <Grid.Column width={8}>
-                  <Modal trigger={<Button color='red' size='massive' fluid>Donate Now</Button>}>
+              <Grid.Row columns={2} centered>
+                <Grid.Column width={9}>
+                  <Image rounded src={test} />
+                  <Modal
+                      trigger={
+                        <Button onClick={this.handleOpen}
+                                color='red'
+                                size='huge'
+                                fluid
+                                attached='bottom'
+                                animated='fade'
+                        >
+                          <Button.Content visible>Donate Now</Button.Content>
+                          <Button.Content hidden>Make a Difference</Button.Content>
+                        </Button>}
+                      open={this.state.modalOpen}
+                      onClose={this.handleClose}
+                  >
                     <Modal.Header>Will you support the Rough Rider Ohana?</Modal.Header>
                     <Modal.Content image>
                       <Image wrapped size='medium' src={ogawa} />
                       <Modal.Description>
                         <Header>Make Riders Successful.</Header>
                         <p>
-                          Your donation will help to set RHS students and alumni up for success through scholarships, campus improvement projects, and grants to support classrooms.
+                          Your donation will directly impact RHS students and alumni. Help to set them up for success through our scholarships, campus improvement projects, and grants to support classrooms.
                         </p>
-                        <p>Donations are handled via PayPal using the yellow button below.</p>
+                        <p>Donations are handled via PayPal using the yellow donate button below.</p>
                         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
                           <input type="hidden" name="cmd" value="_donations" />
                           <input type="hidden" name="business" value="rafhawaii@gmail.com" />
@@ -51,20 +61,38 @@ class Landing extends React.Component {
                       </Modal.Description>
                     </Modal.Content>
                     <Modal.Actions>
-                      <Button color='green' inverted>
+                      <Button color='green' onClick={this.handleClose} inverted>
                         <Icon name='checkmark' /> OK
                       </Button>
                     </Modal.Actions>
                   </Modal>
                 </Grid.Column>
               </Grid.Row>
+              <Grid.Row/>
             </Grid>
           </Container>
-          <Container>
             <Segment style={segmentStyle} raised color='yellow'>
-            The Roosevelt Alumni Foundation (RAF) raises funds to support our alma mater, President Theodore Roosevelt High School, in a variety of ways. These include offering annual scholarships to alumni. We also admit qualified applicants to the Roosevelt Hall of Fame. As a designated non-profit organization, donations to the RAF are tax deductible.
-
-             <br/><br/>The Roosevelt High School Alumni Association (RHSAA) works toward developing a spirit of fellowship among its members in order to rekindle and strengthen ties to our alma mater and each other by publishing newsletters for its members and co-hosting with the RAF an annual ho’olaule’a – a get together for alumni.
+              <Container>
+                <Grid>
+                  <Grid.Column width={4}>
+                    <Header color='red'>RHS Alumni Association</Header>
+                    <Image rounded src={projectGrad}/>
+                    <br/>The Roosevelt High School Alumni Association (RHSAA) works toward developing a spirit of fellowship among its members in order to rekindle and strengthen ties to our alma mater and each other by publishing newsletters for its members and co-hosting with the RAF an annual ho’olaule’a – a get together for alumni.
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    <Header color='red'>Alumni Foundation</Header>
+                    <Image rounded src={projectGrad}/>
+                    <br/>The Roosevelt Alumni Foundation (RAF) raises funds to support our alma mater, President Theodore Roosevelt High School, in a variety of ways. These include offering annual scholarships to alumni. We also admit qualified applicants to the Roosevelt Hall of Fame. As a designated non-profit organization, donations to the RAF are tax deductible.
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    <Header color='red'>School Support</Header>
+                    <Image rounded src={projectGrad}/>
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    <Header color='red'>Friends of Project Grad</Header>
+                    <Image rounded src={projectGrad}/>
+                  </Grid.Column>
+                </Grid>
 
               <br/><br/>In addition to offering scholarships to RHS graduates, the RAF also provides support to Roosevelt High School by providing grants to support the school. Our projects include campus beautification and other needs. The RAF adopted the Friends of Roosevelt Project Grad (“FRPG”) in 2007. As the group’s fiscal sponsor, all donations to FRPG, are also tax deductible.
 
@@ -77,9 +105,9 @@ class Landing extends React.Component {
               <br/><br/>The Roosevelt High School Alumni Association ("RHSAA") was originally chartered in 1975 as Rough Riders -  Hawaii.  In 1979, the name was officially changed to RHSAA.   In 1993, the Roosevelt Alumni Foundation ("RAF") was formed and obtained a non-profit designation - 501c(3), from the Internal Revenue Service in 1996, making all donations to the RAF tax deductible.
 
               <br/><br/>If you want to subscribe to our newsletter or share information about your class/classmates, please download the membership application below and join the RHSAA.   E-mail your favorite memories at Roosevelt to our Newsletter Editor- Pualani Good '60 at: rhsaa.hawaii@gmail.com
-
+                </Container>
               </Segment>
-          </Container>
+
         </div>
 
     )
--- my-app/src/pages/Scholarships.js
+++ my-app/src/pages/Scholarships.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Header, Segment, Image, Container } from 'semantic-ui-react';
+import { Header, Segment, Image, Container, List } from 'semantic-ui-react';
 import '../style.css';
+import scholarshipPhoto from '../images/scholarships-2012.jpg';
 
 class Scholarships extends React.Component {
   render() {
@@ -9,61 +10,61 @@ class Scholarships extends React.Component {
           <Segment>
             <Container>
               <Header>Scholarships for Roosevelt Graduates</Header>
+              We seek to provide financial aid to graduates of Roosevelt High School who demonstrate the ability, desire, and have a financial need to be met in order to pursue academic, trade or vocational school goals.
+              <Image src={scholarshipPhoto} size='large' centered rounded/>
               The Roosevelt Alumni Foundation has contributed over $70,000 in scholarships to both hard-working and need-based students (not necessarily determined by federal guidelines).
 
-              The purpose of this program is to provide financial aid to graduates of Roosevelt High School who demonstrate the ability, desire and have a financial need to be met in order to pursue academic, trade or vocational school goals.
+              <Header>Scholarship Requirements</Header>
+              <List bulleted>
+                <List.Item>Be Cool</List.Item>
+                <List.Item>Stay In School</List.Item>
+              </List>
 
               <Header>2016 RAF Scholarship Winners</Header>
               In 2016, with the assistance of the Roosevelt Alumni Foundation, the Class of 1961 and the Friends of Red & Gold, we awarded 10 scholarships for a total of $10,000!
 
               2016 RAF
-
-              Kevin Liu,           $1,000 University of Hawaii, Manoa
-              Itsuka Nomi,      $1,000 University of Hawaii, Manoa
-
-              Lynda Valen,      $1,000 University of Hawaii, Manoa
-
+              <List bulleted>
+                <List.Item>Kevin Liu, $1,000 University of Hawaii, Manoa</List.Item>
+                <List.Item>Itsuka Nomi, $1,000 University of Hawaii, Manoa</List.Item>
+                <List.Item>Lynda Valen, $1,000 University of Hawaii, Manoa</List.Item>
+              </List>
               2016 RAF & Class of ‘61
-
-              Arong Lee,                 $1,000 Whitman College
-
-              Kim Yen Nguyen,      $1,000 University of Hawaii, Manoa
-
-              Rachelle Anne Silao, $1,000 Chaminade University
+              <List bulleted>
+                <List.Item>Arong Lee, $1,000 Whitman College</List.Item>
+                <List.Item>Kim Yen Nguyen, $1,000 University of Hawaii, Manoa</List.Item>
+                <List.Item>Rachelle Anne Silao, $1,000 Chaminade University</List.Item>
+              </List>
 
               2016 RAF Friends of Red & Gold
-
-              Yuwen Hu            $1,000 University of San Francisco
-
-              Xiaofei MAI            $1,000 University of Hawaii, Manoa
-
-              Gha Ming NG            $1,000 Western Oregon University
-
-              Xingyin PANG            $1,000 University of Hawaii, Manoa
-
-
+              <List bulleted>
+                <List.Item>Yuwen Hu, $1,000 University of San Francisco</List.Item>
+                <List.Item>Xiaofei Mai, $1,000 University of Hawaii, Manoa</List.Item>
+                <List.Item>Gha Ming Ng, $1,000 Western Oregon University</List.Item>
+                <List.Item>Xingyin Pang, $1,000 University of Hawaii, Manoa</List.Item>
+              </List>
 
               2015 Class of '62 and Class of '64
-              Alicia Leong, $1,000, University of Hawaii, Manoa
-              Stephen Radkov, $500, Honolulu Community College
-              Connie Wong, $1,000, University of Hawaii, Manoa
-              Kristen Kaulia, $1,500, Eastern Washington University
-              Joanne Huang, $1,000, Lehigh University
-              Chin Huynh, $1,000, Gonzaga University
-              Kaohi Kapiko, $1,000,Gonzaga University
-              Jake Kawasaki, $1,000, Santa Clara University
-              Tammy Ko, $1,000, University of Southern California
-              Kristen Kojima, $1,000, Creighton University
-              Colleen Lau, $1,000, University of Hawaii, Manoa
-              Jason Lin, $1,000, American University
-              Aaron Miyasato, $1,000, University of Washington
-              Brenda Nguyen, $1,000, University of Hawaii, Manoa
-              Germiniano Sarmiento III, $1,000, Willamette University
-              Yanna Xian, $1,000, Creighton University
-              Hinman Zhou, $1,100, Creighton University
-              Kai Tsubota, $1,500, Whitman University
-
-
+              <List bulleted>
+                <List.Item>Alicia Leong, $1,000, University of Hawaii, Manoa</List.Item>
+                <List.Item>Stephen Radkov, $500, Honolulu Community College</List.Item>
+                <List.Item>Connie Wong, $1,000, University of Hawaii, Manoa</List.Item>
+                <List.Item>Kristen Kaulia, $1,500, Eastern Washington University</List.Item>
+                <List.Item>Joanne Huang, $1,000, Lehigh University</List.Item>
+                <List.Item>Chin Huynh, $1,000, Gonzaga University</List.Item>
+                <List.Item>Kaohi Kapiko, $1,000,Gonzaga University</List.Item>
+                <List.Item>Jake Kawasaki, $1,000, Santa Clara University</List.Item>
+                <List.Item>Tammy Ko, $1,000, University of Southern California</List.Item>
+                <List.Item>Kristen Kojima, $1,000, Creighton University</List.Item>
+                <List.Item>Colleen Lau, $1,000, University of Hawaii, Manoa</List.Item>
+                <List.Item>Jason Lin, $1,000, American University</List.Item>
+                <List.Item>Aaron Miyasato, $1,000, University of Washington</List.Item>
+                <List.Item>Brenda Nguyen, $1,000, University of Hawaii, Manoa</List.Item>
+                <List.Item>Germiniano Sarmiento III, $1,000, Willamette University</List.Item>
+                <List.Item>Yanna Xian, $1,000, Creighton University</List.Item>
+                <List.Item>Hinman Zhou, $1,100, Creighton University</List.Item>
+                <List.Item>Kai Tsubota, $1,500, Whitman University</List.Item>
+              </List>
             </Container>
           </Segment>
         </div>
