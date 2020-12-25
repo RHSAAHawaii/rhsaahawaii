b62ea46678a83e652768bad733db78bca49efe19
junhocs <69886520+junhocs@users.noreply.github.com>
2020-12-24 14:24:09 -1000

    Created BoardCard.jsx and Added Underscore to Package.json

---
+++ my-app/src/components/BoardCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import '../style.css';
+
+const srcRHSLogo = 'http://rhsaahawaii.org/wp-content/uploads/2019/04/cropped-logo2-2.png';
+
+class BoardCard extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default BoardCard;
