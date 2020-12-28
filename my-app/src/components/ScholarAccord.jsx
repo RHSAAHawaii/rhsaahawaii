fd0a48feab30472bfe52de1cefc62ff2b6757b9e
Jun Ho Im <69886520+junhocs@users.noreply.github.com>
2020-12-28 00:25:58 -1000

    Scholarships (#15)
    * added list of scholars
    
    * added some scholars
    
    i am currently trying to render the scholars through the accordion component instead of hard coding it but i cant properly filter the data to have the students match the years
    
    * WIP until i get to it
    
    * Made accordians in scholarships page dynamic
    
    * Rename scholarAccord.jsx to ScholarAccord.jsx
    
    Co-authored-by: Jin <jinyan@hawaii.edu>

---
+++ my-app/src/components/ScholarAccord.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Accordion, Card, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import '../style.css';
+import ScholarAccordBody from './ScholarAccordBody';
+import _ from 'underscore/underscore-esm';
+
+class ScholarAccord extends React.Component {
+  render() {
+    return (
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey={this.props.classInfo}>Class of {this.props.classInfo}</Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey={this.props.classInfo}>
+                  <Card.Body>
+                    {_.map(this.props.scholarInfo, (current, index) => <ScholarAccordBody key={index} info={current}/> )}
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+    )
+  } 
+}
+
+ScholarAccord.propTypes = {
+  scholarInfo: PropTypes.array.isRequired,
+  classInfo: PropTypes.string.isRequired,
+};
+
+export default ScholarAccord;
--- my-app/src/pages/Scholarships.jsx
+++ my-app/src/pages/Scholarships.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../style.css';
 import scholarshipPhoto from '../images/scholarships-2012.jpg';
+import scholars from '../data/scholars.json';
+import _ from 'underscore/underscore-esm';
+import ScholarAccord from '../components/ScholarAccord';
 import {
   Accordion,
-  Card,
-  Button,
   Container,
   Image,
   Row,
@@ -14,8 +15,7 @@ import {
 
 class Scholarships extends React.Component {
   render() {
-
-
+    const arrayInfo = (_.groupBy(scholars, 'Class'));
     return (
         <div>
           <h1 className="impactHeader">Scholarships</h1>
@@ -38,7 +38,7 @@ class Scholarships extends React.Component {
               </Row>
             </Container>
           </Jumbotron>
-          <Container>
+          <Container style={{marginBottom: 25}}>
             <h2 style={{ marginTop: "50px" }}>Scholarship Requirements</h2>
             <ul>
               <li>Be cool</li>
@@ -48,63 +48,10 @@ class Scholarships extends React.Component {
                 This includes institutions that offer courses for study of a trade of a trade or vocation.
               </li>
             </ul>
-
             <h2 style={{ marginTop: "50px" }}>Past Scholarship Winners</h2>
-            <Accordion defaultActiveKey="0">
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    2016 RAF Scholarship Winners
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    <ul>
-                      <li>Yuwen Hu, $1,000 University of San Francisco</li>
-                      <li>Arong Lee, $1,000 Whitman College</li>
-                      <li>Kevin Liu, $1,000 University of Hawaii, Manoa</li>
-                      <li>Xiaofei Mai, $1,000 University of Hawaii, Manoa</li>
-                      <li>Gha Ming Ng, $1,000 Western Oregon University</li>
-                      <li>Kim Yen Nguyen, $1,000 University of Hawaii, Manoa</li>
-                      <li>Itsuka Nomi, $1,000 University of Hawaii, Manoa</li>
-                      <li>Yingyin Pang, $1,000 University of Hawaii, Manoa</li>
-                      <li>Rachelle Anne Silao, $1,000 Chaminade University</li>
-                      <li>Lynda Valen, $1,000 University of Hawaii, Manoa</li>
-                    </ul>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                    2015 Class of '62 and Class of '64
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>
-                    <ul>
-                      <li>Joanne Huang, $1,000, Lehigh University</li>
-                      <li>Chin Huynh, $1,000, Gonzaga University</li>
-                      <li>Kaohi Kapiko, $1,000,Gonzaga University</li>
-                      <li>Kristen Kaulia, $1,500, Eastern Washington University</li>
-                      <li>Jake Kawasaki, $1,000, Santa Clara University</li>
-                      <li>Tammy Ko, $1,000, University of Southern California</li>
-                      <li>Kristen Kojima, $1,000, Creighton University</li>
-                      <li>Colleen Lau, $1,000, University of Hawaii, Manoa</li>
-                      <li>Alicia Leong, $1,000, University of Hawaii, Manoa</li>
-                      <li>Jason Lin, $1,000, American University</li>
-                      <li>Aaron Miyasato, $1,000, University of Washington</li>
-                      <li>Brenda Nguyen, $1,000, University of Hawaii, Manoa</li>
-                      <li>Stephen Radkov, $500, Honolulu Community College</li>
-                      <li>Germiniano Sarmiento III, $1,000, Willamette University</li>
-                      <li>Kai Tsubota, $1,500, Whitman University</li>
-                      <li>Connie Wong, $1,000, University of Hawaii, Manoa</li>
-                      <li>Yanna Xian, $1,000, Creighton University</li>
-                      <li>Hinman Zhou, $1,100, Creighton University</li>
-                    </ul>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
+            <Accordion defaultActiveKey='0'>
+              {_.map(arrayInfo, (current, index)=> <ScholarAccord classInfo={index}
+              scholarInfo={current} />).reverse()}
             </Accordion>
           </Container>
         </div>
