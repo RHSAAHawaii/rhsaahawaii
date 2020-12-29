a92eed3b560d1299e88bbbe7e2d7685fb2dc2f74
saharama <test-hellow@hawaii.edu>
2020-12-28 17:44:54 -1000

    updated donate button

--- my-app/src/pages/Donate.jsx
+++ my-app/src/pages/Donate.jsx
@@ -21,17 +21,7 @@ class Donate extends React.Component {
                   <input type="hidden" name="business" value="rafhawaii@gmail.com"/>
                   <input type="hidden" name="item_name" value="Roosevelt Alumni Association Donation" />
                   <input type="hidden" name="currency_code" value="USD" />
-                  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0"  name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                  <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-                </form>
-              </p>
-              <p>
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" >
-                  <input type="hidden" name="cmd" value="_donations" />
-                  <input type="hidden" name="business" value="rafhawaii@gmail.com"/>
-                  <input type="hidden" name="item_name" value="Roosevelt Alumni Association Donation" />
-                  <input type="hidden" name="currency_code" value="USD" />
-                  <button type="submit" class="btn btn-primary">Donate</button>
+                  <button type="submit" class="btn btn-primary btn-lg">Donate Here</button>
                 </form>
               </p>
             </Jumbotron>
