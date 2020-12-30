b8cac1af088e7c2ddc323e29fea2e461172a4ac8
Jin <jinyan@hawaii.edu>
2020-12-29 16:57:43 -1000

    Testing new fonts

--- my-app/src/pages/Scholarships.jsx
+++ my-app/src/pages/Scholarships.jsx
@@ -13,7 +13,8 @@ import {
   Image,
   Row,
   Col,
-  Jumbotron, Carousel
+  Jumbotron,
+  Carousel
 } from 'react-bootstrap';
 
 class Scholarships extends React.Component {
@@ -23,12 +24,11 @@ class Scholarships extends React.Component {
 
     return (
         <div>
-          <h1 className="impactHeader">Scholarships</h1>
+          <Container><h1 className="scholarHeader">SCHOLARSHIPS</h1></Container>
           <Jumbotron>
             <Container>
               <Row>
-                <Col><Image rounded width='100%' src={scholarshipPhoto}/></Col>
-                <Col>
+                <Col className="col-7">
                   <p style={{fontSize: 20}}>We seek to provide financial aid to
                     graduates of Roosevelt High School
                     who demonstrate the ability, desire,
@@ -41,32 +41,20 @@ class Scholarships extends React.Component {
                     need-based students (not necessarily determined by federal guidelines).
                   </p>
                 </Col>
+                <Col className="col-5"><Image rounded width='100%' src={scholarshipPhoto}/></Col>
               </Row>
             </Container>
           </Jumbotron>
           <Container style={{ marginBottom: 25 }}>
             <Row>
-              <Col>
-                <h2 style={subHeaders}>Scholarship Requirements</h2>
-                <ul>
-                  <li>Be cool</li>
-                  <li>Stay in school</li>
-                  <li>Be a graduating student or graduate of Roosevelt High School</li>
-                  <li>Submit evidence of acceptance into an accredited institution of higher learning.
-                    This includes institutions that offer courses for study of a trade of a trade or vocation.
-                  </li>
-                </ul>
-              </Col>
-              <Col>
-              {/* Something here ?*/}
-              </Col>
-              <Col>
+              <Col className="col-4">
                 <h2 style={subHeaders}>Recent Recipients</h2>
                 <Carousel>
                   <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={calyxB}
+                        alt="first slide"
                     />
                     <Carousel.Caption>
                       <p>Calyx Badon</p>
@@ -76,6 +64,7 @@ class Scholarships extends React.Component {
                     <img
                         className="d-block w-100"
                         src={lakotaN}
+                        alt="second slide"
                     />
                     <Carousel.Caption>
                       <p>Lakota Nguyen</p>
@@ -85,12 +74,27 @@ class Scholarships extends React.Component {
                     <img
                         className="d-block w-100"
                         src={maxH}
+                        alt="third slide"
                     />
                     <Carousel.Caption>
                       <p>Maxwell Higa</p>
                     </Carousel.Caption>
                   </Carousel.Item>
                 </Carousel>
+              </Col>
+              <Col className="col-8">
+                <h2 style={subHeaders}>Scholarship Requirements</h2>
+                <ul style={{fontSize: 16}}>
+                  <li>Be cool</li>
+                  <br/>
+                  <li>Stay in school</li>
+                  <br/>
+                  <li>Be a graduating student or graduate of Roosevelt High School</li>
+                  <br/>
+                  <li>Submit evidence of acceptance into an accredited institution of higher learning.
+                    This includes institutions that offer courses for study of a trade of a trade or vocation.
+                  </li>
+                </ul>
               </Col>
             </Row>
             <h2 style={{ marginTop: 50 }}>Past Scholarship Winners</h2>
