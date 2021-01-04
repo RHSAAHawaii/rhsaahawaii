50df62453f291b4f6f9eff918c93b2bce6c819d9
Jin <jinyan@hawaii.edu>
2021-01-03 16:05:08 -1000

    Updated new layout

--- my-app/src/pages/Impact.jsx
+++ my-app/src/pages/Impact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../style.css';
+import RooseveltFront from '../images/RooseveltFront.jpg';
 import { Container, Jumbotron, Row, Col, Image } from 'react-bootstrap';
 
 class Impact extends React.Component {
@@ -7,30 +8,45 @@ class Impact extends React.Component {
   render() {
     return (
         <div>
-          <Container><h1 className='scholarHeader'>ROOSEVELT ALUMNI FOUNDATION</h1></Container>
-          <div className="rooseveltFront"><img style={{ height: "430px" }}/></div>
+          <div className="d-flex align-items-center rooseveltFront">
+            <div
+                className="container impactTransparentTitleBox text-center p-5 animate__animated animate__fadeIn">
+              <h1>Roosevelt Alumni Foundation</h1>
+            </div>
+          </div>
 
           <Container>
-            <div>
-              <h2 className='impactContent2'>Origins</h2>
-              <p>
-                The Roosevelt High School Alumni Association ("RHSAA") was originally chartered in 1975 as Rough Riders
-                -
-                Hawaii. In 1979, the name was officially changed to RHSAA. In 1993, the Roosevelt Alumni Foundation
-                ("RAF") was formed and obtained a non-profit designation - 501c(3), from the Internal Revenue Service in
-                1996, making all donations to the RAF tax deductible.
-              </p>
+            <Row>
+              <Col sm="4" className="bg-light mt-5 p-5">
+                <h1 className="impactSubheaders">Get Involved</h1>
+                <hr className="yellowHr"/>
+                <p className="mt-4">
+                  Membership <br/>
+                  Events <br/>
+                  Donate
+                </p>
+              </Col>
+              <Col className="impactYellowBox mt-5 p-5" sm="8">
+                <h1 className="impactSubheaders">Origins</h1>
+                <hr className="redHr"/>
+                <p className="mt-4">The Roosevelt High School Alumni Association ("RHSAA") was originally chartered in
+                  1975 as Rough Riders
+                  - Hawaii. In 1979, the name was officially changed to RHSAA. In 1993, the Roosevelt Alumni Foundation
+                  ("RAF") was formed and obtained a non-profit designation - 501c(3), from the Internal Revenue Service
+                  in
+                  1996, making all donations to the RAF tax deductible.</p>
+              </Col>
+            </Row>
+          </Container>
 
-              <h2 className='impactContent2'>Fellowship</h2>
-              The Roosevelt High School Alumni Association (RHSAA) works toward developing a spirit of fellowship
-              among
+          <Container className="p-3 mt-5">
+            <h1 className="impactSubheaders">Fellowship</h1>
+            <p className="lead">The Roosevelt High School Alumni Association (RHSAA) works toward developing a spirit of fellowship among
               its members in order to rekindle and strengthen ties to our alma mater and each other by publishing
               newsletters for its members and co-hosting with the RAF an annual ho’olaule’a – a get together for
-              alumni.
-            </div>
-
-            <h2 className='impactContent2'>Scholarship</h2>
-            <p>
+              alumni.</p>
+            <h1 className="impactSubheaders" style={{ marginTop: 50 }}>Scholarship</h1>
+            <p className="lead">
               The purpose of the RAF’s scholarship program is to provide financial aid to graduates of Roosevelt
               High School who demonstrate ability, desire and have a financial need to pursue academic, trade or
               vocational school goals in Hawaii.
@@ -40,8 +56,8 @@ class Impact extends React.Component {
               sponsor, all donations to FRPG, are also tax deductible.
             </p>
 
-            <h2 className='impactContent2'>Contact</h2>
-            <p>
+            <h1 className='impactSubheaders'>Contact</h1>
+            <p className="lead" style={{ marginBottom: 50 }}>
               If you want to subscribe to our newsletter or share information about your class/classmates, please
               download the membership application below and join the RHSAA. E-mail your favorite memories at Roosevelt
               to our Newsletter Editor- Pualani Good '60 at: rhsaa.hawaii@gmail.com
